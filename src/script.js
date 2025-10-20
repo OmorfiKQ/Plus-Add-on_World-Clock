463656dcfcba6927ae0f4b3222bb90c56ac07398
OmorfiKQ <okqcares@gmail.com>
2025-10-20 16:27:36 -0400

    added js to selectElement

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 function updateTime() {
   // tab-1 elements
   let cityOneElement = document.querySelector("#city-one");
+  if (cityOneElement) {
   let cityOneDateElement = cityOneElement.querySelector(".date");
   let cityOneTimeElement = cityOneElement.querySelector(".time");
   let cityOneTime = moment().tz("Europe/Moscow");
@@ -8,12 +9,13 @@ function updateTime() {
   cityOneDateElement.innerHTML = cityOneTime.format("dddd, MMMM Do YYYY");
   cityOneTimeElement.innerHTML = cityOneTime.format(
     "h:mm:ss [<small>]A[</small>]"
-  );
+  );}
 
 
   // tab-2 elements
 
   let cityTwoElement = document.querySelector("#city-two");
+   if (cityTwoElement) {
   let cityTwoDateElement = cityTwoElement.querySelector(".date");
   let cityTwoTimeElement = cityTwoElement.querySelector(".time");
   let cityTwoTime = moment().tz("Japan");
@@ -21,8 +23,33 @@ function updateTime() {
   cityTwoDateElement.innerHTML = cityTwoTime.format("dddd, MMMM Do YYYY");
   cityTwoTimeElement.innerHTML = cityTwoTime.format(
     "h:mm:ss [<small>]A[</small>]"
-  );
+  );}
 }
+
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+
+  citiesElement.innerHTML = ` <div class="cities">
+        <div class="tabs">
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("dddd, MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss"
+        )} <small>${cityTime.format("A")}</small></div>
+      </div>`;
+  } 
 
 updateTime();
 setInterval(updateTime, 1000);
+
+
+
+
+let citiesSelectElement = document.querySelector("#city-select");
+
+citiesSelectElement.addEventListener("change", updateCity);
